6d1c461f7064d949ff6bb588b07163e3d97c6c6e
Iryna Shevkun <sevkunirina65@gmail.com>
2025-04-29 13:12:38 +0300

    make build

---
+++ index.74c1b71b.js
@@ -0,0 +1,2 @@
+var t=document.querySelector("div"),e=document.querySelector("img"),c=t.scrollHeight/2-25,l=t.scrollWidth/2-25;e.classList.contains("spider")&&(e.style.top="".concat(c,"px"),e.style.left="".concat(l,"px"));
+//# sourceMappingURL=index.74c1b71b.js.map
